e932f4e5f1f72821c9ee85fcdd2a8175929cc1f2
SocratesCrespo <socratescrespo@gmail.com>
2014-06-17 21:53:06 -0430

    Cambio en la puntuacion
    Estos son los cambios en el archivo java y en el html.

--- java.js
+++ java.js
@@ -1,13 +1,18 @@
+var contador= 0;
 function one(obj) 
 {
     obj.style.backgroundColor="#32CD32";
     obj.innerHTML="Correcto";
+      contador++;
+    alert("Tu puntuación es: "+contador);
 }
 
 function two(obj) 
 {
    obj.style.backgroundColor="red";
    obj.innerHTML="Incorrecto";
+   contador+0;
+   alert("Tu puntuación es: "+contador)
 }
 
 $(document).ready(function(){
@@ -18,7 +23,7 @@ $(document).ready(function(){
 
 $(document).on('ready',function (){
         $('button').on('click',function(){
-          var nuevo_e = $('<div id="buy"><h2>Estoy jugando</h2><div id="botones"><button id="test" class="p" onclick="one(this)";>Im playing</button><button id="test"class="p" onclick="one(this)";>Im playing</button><button id="test"class="p" onclick="one(this)";>Im playing</button><button id="test"class="p" onclick="one(this)";>Im playing</button></div></div>')
+          var nuevo_e = $('<div id="buy"><h2>Estoy jugando</h2><div id="botones"><button id="test" class="p" onclick="two(this)";>Im flaying</button><button id="test"class="p" onclick="one(this)";>Im playing</button><button id="test"class="p" onclick="two(this)";>Im working</button><button id="test"class="p" onclick="two(this)";>Im makeing</button></div></div>')
           $('article').append(nuevo_e)  
         })
 
